44638012ca5174111745fae4c4b751bb5c40516a
Mary Cotter <marebearc@gmail.com>
2018-07-30 14:22:59 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,17 @@
 # Code your cash register here!
+class AmazonTransaction
+  attr_accessor :total, :items, :add_item
+  attr_reader :discount
+  def initialize(discount=0)
+    @total = 0 
+    @items = []
+    @discount = discount 
+  end
+  def add_item(title, price, quantity=1)
+    @total += price * quantity
+    @items << title
+  end
+  def apply_discount(discount)
+    @total *= discount
+  end
+end
