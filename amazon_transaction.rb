b5f405a187374e308475f7d99208f85cfe1d5412
Mary Cotter <marebearc@gmail.com>
2018-07-30 14:29:57 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -11,7 +11,7 @@ class AmazonTransaction
     @total += price * quantity
     @items << title
   end
-  def apply_discount
+  def apply_discount(discount)
     @total = ((100 - @discount)/ 100) * @total
   end
 end
